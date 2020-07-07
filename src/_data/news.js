d459676c57f2d922124fd5efa3e13507d1567b95
yoshi <yoshi@yoshis-MacBook-Pro.local>
2020-07-07 12:38:00 -0400

    added localization

--- src/_data/news.js
+++ src/_data/news.js
@@ -1,14 +1,26 @@
 const axios = require("axios");
+const countries = require("./countries.json");
 require("dotenv").config();
 
-module.exports = async function () {
+async function getNews(country) {
   try {
     const response = await axios.get(
-      `https://newsapi.org/v2/top-headlines?country=us&apiKey=${process.env.NEWS_API_KEY}&pageSize=5`
+      `https://newsapi.org/v2/top-headlines?country=${country}&apiKey=${process.env.NEWS_API_KEY}&pageSize=5`
     );
-    return response.data;
+    return {
+      country: country,
+      articles: response.data.articles,
+    };
   } catch (error) {
     console.error(error);
     //dummy test
   }
+}
+
+module.exports = async function () {
+  var newsPromises = countries.map(getNews);
+  return Promise.all(newsPromises).then((newsObjects) => {
+    console.log("newsObjects", newsObjects);
+    return [].concat.apply([], newsObjects);
+  });
 };
