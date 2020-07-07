1a13323cac8844993ff5e3ef3f188dc00c724142
yoshi <yoshi@yoshis-MacBook-Pro.local>
2020-07-07 11:05:49 -0400

    changed env var

--- src/_data/news.js
+++ src/_data/news.js
@@ -4,7 +4,7 @@ require("dotenv").config();
 module.exports = async function () {
   try {
     const response = await axios.get(
-      `https://newsapi.org/v2/top-headlines?country=us&apiKey=${process.env.API_KEY}&pageSize=5`
+      `https://newsapi.org/v2/top-headlines?country=us&apiKey=${process.env.NEWS_API_KEY}&pageSize=5`
     );
     return response.data;
   } catch (error) {
